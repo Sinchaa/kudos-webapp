52d3a845682c57e0557ee27c73afaa86eed245e6
Sinchana <sinchanashanbhag123@gmail.com>
2025-11-11 15:33:45 +0530

    fixing restructuring

--- src/components/shared/Avatar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { getInitials } from '../../utils/helpers';
-
-export const Avatar = ({ firstName, lastName, username }) => (
-  <div className="user-avatar">
-    {getInitials(firstName, lastName, username)}
-  </div>
-);
